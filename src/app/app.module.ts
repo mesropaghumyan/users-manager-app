aa653fc30d7c05a1b5b755fea4b3199cb513aab0
mesropaghumyan <mesrop.aghumyan@etu.univ-lyon1.fr>
2023-12-16 15:13:53 +0100

    inital commit

---
+++ src/app/app.module.ts
@@ -0,0 +1,22 @@
+import { NgModule } from '@angular/core';
+import { BrowserModule } from '@angular/platform-browser';
+
+import { AppComponent } from './app.component';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+import { AppRoutingModule } from './app-routing.module';
+import {RouterOutlet} from "@angular/router";
+
+@NgModule({
+  declarations: [
+    AppComponent
+  ],
+    imports: [
+        BrowserModule,
+        BrowserAnimationsModule,
+        AppRoutingModule,
+        RouterOutlet
+    ],
+  providers: [],
+  bootstrap: [AppComponent]
+})
+export class AppModule { }
