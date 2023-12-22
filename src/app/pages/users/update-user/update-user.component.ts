818c859b298724cf40bc8a33c1fa4ae83d09ce36
mesropaghumyan <mesrop.aghumyan@etu.univ-lyon1.fr>
2023-12-22 23:04:44 +0100

    feat(form): add all from component feats

--- src/app/pages/users/update-user/update-user.component.ts
+++ src/app/pages/users/update-user/update-user.component.ts
@@ -1,36 +1,9 @@
 import {Component, OnInit} from '@angular/core';
-import {FormBuilder, FormControl, FormGroup, Validators} from "@angular/forms";
+import {FormBuilder} from "@angular/forms";
 import {ActivatedRoute, Router} from "@angular/router";
 import {UserInterface} from "../user.interface";
+import {UserService} from "../user.service";
 import {Subscription} from "rxjs";
-
-type UpdateFormGroupInterface = FormGroup<{
-  name: FormControl<string | null>,
-  email: FormControl<string | null>,
-  password: FormControl<string | null>,
-  occupation: FormControl<string | null>,
-  bio: FormControl<string | null>,
-}>;
-
-const UPDATE_FORM_VALIDATORS = {
-  name: [null as unknown as string, [
-    Validators.required,
-  ]],
-  email: [null as unknown as string, [
-    Validators.required,
-    Validators.pattern(/^[a-zA-Z0-9._%+-]+@[a-zA-Z0-9.-]+\.[a-zA-Z]{2,}$/)
-  ]],
-  password: [null as unknown as string, [
-    Validators.required,
-    Validators.minLength(8)
-  ]],
-  occupation: [null as unknown as string, [
-    Validators.required,
-  ]],
-  bio: [null as unknown as string, [
-    Validators.required,
-  ]],
-}
 
 @Component({
   selector: 'app-update-user',
@@ -38,29 +11,24 @@ const UPDATE_FORM_VALIDATORS = {
   styleUrls: ['./update-user.component.css']
 })
 export class UpdateUserComponent implements OnInit {
-  userId: string | null = null;
-  updateFormGroup: UpdateFormGroupInterface;
-  userUpdate: UserInterface | null = null;
-  userUpdateSubscription!: Subscription;
+  userId!: string;
+  updateUser!: UserInterface;
 
-  name: string | undefined = "";
-  email: string | undefined = "";
-  password: string | undefined = "";
-  occupation: string | undefined = "";
-  bio: string | undefined = "";
-
-  constructor(private activatedRoute: ActivatedRoute, private formBuilder: FormBuilder, private router: Router) {
-    this.updateFormGroup = this.formBuilder.group(UPDATE_FORM_VALIDATORS)
+  constructor(private activatedRoute: ActivatedRoute, private userService: UserService) {
   }
 
   ngOnInit(): void {
-    this.userId = this.activatedRoute.snapshot.paramMap.get('id');
+    this.userId = this.activatedRoute.snapshot.paramMap.get('id') || '';
 
-    this.updateFormGroup.valueChanges.subscribe((value: Partial<{name: string | null, email: string | null, password: string | null, occupation: string | null, bio: string | null}>): void => {
-    });
-  }
-
-  onSubmitUpdateForm(): void {
-
+    if (this.userId) {
+      this.userService.getById(this.userId).subscribe(
+        (response) => {
+          this.updateUser = response;
+        },
+        (error) => {
+          console.log(error);
+        }
+      )
+    }
   }
 }
---
+++ src/app/pages/users/user-form/user-form.component.ts
@@ -0,0 +1,79 @@
+import {Component, Input, OnInit} from '@angular/core';
+import {NgForm} from "@angular/forms";
+import {UserInterface} from "../user.interface";
+import {UserService} from "../user.service";
+import {Router} from "@angular/router";
+
+@Component({
+  selector: 'app-user-form',
+  templateUrl: './user-form.component.html',
+  styleUrls: ['./user-form.component.css']
+})
+export class UserFormComponent implements OnInit {
+  @Input({required: true}) mode!: 'add' | 'update';
+  @Input({required: false}) name!: string;
+  @Input({required: false}) email!: string;
+  @Input({required: false}) password!: string;
+  @Input({required: false}) occupation!: string;
+  @Input({required: false}) bio!: string;
+  @Input({required: false}) id!: string;
+
+  @Input({required: false}) user!: UserInterface;
+
+  public title!: string;
+  public btnTitle!: string;
+
+  constructor(private userService: UserService, private router: Router) {
+  }
+
+  ngOnInit(): void {
+    switch (this.mode) {
+      case ("update"):
+        this.title = "Update user";
+        this.btnTitle = "Update";
+        break;
+      case ("add"):
+        this.title = "Add user";
+        this.user = this.userService.initializeEmptyUser();
+        this.btnTitle = "Add";
+    }
+  }
+
+  onSubmitUserForm(f: NgForm): void {
+    if (f.valid) {
+      switch (this.mode) {
+        case ("update"):
+          this.userService.updateUser(this.id, this.user).subscribe(
+            (response) => {
+              const updatedUsers = this.userService.users.value ? [...this.userService.users.value] : [];
+              const index = updatedUsers.findIndex(u => u.id === this.id);
+              if (index !== -1) {
+                updatedUsers[index] = response;
+                this.userService.users.next(updatedUsers);
+                this.router.navigate(['/']);
+                // TODO: Redirect to error page
+              }
+            },
+            (error) => {
+              console.log(error);
+            }
+          )
+          break;
+        case ("add"):
+          this.user = this.userService.mapperUser(f.value);
+          this.userService.addUser(this.user).subscribe(
+            (response) => {
+              const updatedUsers: UserInterface[] = [...(this.userService.users.value || []), response];
+              this.userService.users.next(updatedUsers);
+              this.router.navigate(['/']);
+              // TODO: Redirect to successfully page
+            },
+            (error) => {
+              console.log(error);
+            }
+          )
+          break;
+      }
+    }
+  }
+}
